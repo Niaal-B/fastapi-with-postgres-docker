25571cfcf5e0df42827e91d4c7e49d5c1c0b4906
Niaal-B <nihnihal504@gmail.com>
2025-04-26 12:31:00 +0530

    refactor: implemented utils.py file to hash password

--- app/main.py
+++ app/main.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI, Response, HTTPException, Depends, status
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from typing import List
-from passlib.context import CryptContext
 
-from . import models, schemas
+from . import models, schemas,utils
 from .database import engine, get_db
 
-pwd_context = CryptContext(schemes=["bcrypt"],deprecated='auto')
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -91,7 +89,7 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 @app.post("/user",status_code=status.HTTP_201_CREATED,response_model=schemas.UserOut)
 def create_user(user:schemas.UserCreate,db: Session = Depends(get_db)):
 
-    hashed_password = pwd_context.hash(user.password)
+    hashed_password = utils._hash(user.password)
     user.password = hashed_password
     print(user.password)
     new_user = models.User(**user.model_dump())
