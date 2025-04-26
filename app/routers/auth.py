977f0b2ff994b6201f222b059512fe1ced61c994
Niaal-B <nihnihal504@gmail.com>
2025-04-26 16:42:10 +0530

    fix: added the jwt token creation in the login api

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter,Depends,status,HTTPException,Response
 from sqlalchemy.orm import Session
-from .. import database,schemas,models,utils
+from .. import database,schemas,models,utils,oauth2
 
 
 router = APIRouter(tags=['Authentication'])
@@ -9,10 +9,12 @@ router = APIRouter(tags=['Authentication'])
 def login(user_credentials: schemas.UserLogin,db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User .email==user_credentials.email).first()
     if not user:
-        raise HTTPException(status_code==status.HTTP_404_NOT_FOUND,detail="Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Invalid Credentials")
 
     if not utils.verify(user_credentials.password,user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid Credentials")
 
+    access_token = oauth2.create_access_token(data={"user_id" : user.id})
 
-    return {"token":"Example token"}
+
+    return {"access_token":access_token,"token_type":"bearer"}
