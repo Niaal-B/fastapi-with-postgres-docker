3ffaf9273a1f335360a9c77b30edf3e1fd7d508d
Niaal-B <nihnihal504@gmail.com>
2025-04-27 15:10:33 +0530

    fix: fixed the delete post api and update post api

--- app/routers/post.py
+++ app/routers/post.py
@@ -16,8 +16,8 @@ def get_posts(db: Session = Depends(get_db),current_user:int =  Depends(oauth2.g
 
 # Create a new post
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),user_id:int =  Depends(oauth2.get_current_user)):
-    new_post = models.Post(**post.model_dump())                       
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),current_user:int =  Depends(oauth2.get_current_user)):
+    new_post = models.Post(owner_id=current_user.id,**post.model_dump())                       
     db.add(new_post)
     db.commit()
     db.refresh(new_post)                           
@@ -34,20 +34,29 @@ def get_post(id: int, db: Session = Depends(get_db),current_user:int =  Depends(
 # Delete a post
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db),current_user:int =  Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if post.first() is None:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+
+    if post == None:
         raise HTTPException(status_code=404, detail=f"Post with id {id} not found")
-    post.delete(synchronize_session=False)
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Not authorized to perform requested action")
+
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 # Update a post
 @router.put("/posts/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db),user_id:int =  Depends(oauth2.get_current_user)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db),current_user:int =  Depends(oauth2.get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
     if post is None:
         raise HTTPException(status_code=404, detail=f"Post with id {id} does not exist")
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Not authorized to perform requested action")
     post_query.update(updated_post.model_dump(), synchronize_session=False)
     db.commit()
     return post_query.first()
