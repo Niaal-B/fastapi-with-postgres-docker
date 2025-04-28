ed163dcf545089c3400562832dd9f069ef2081ba
Niaal-B <nihnihal504@gmail.com>
2025-04-28 15:58:55 +0530

    feat: added the logic to implement the like functionality

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel,EmailStr
 from datetime import datetime
 from typing import Optional
-
+from pydantic.types import conint
 
 class PostBase(BaseModel):
     title : str
@@ -47,3 +47,9 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[int] = None
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
+
